b608c596d8605bf18dc7df91aaa49767d8da7ddb
Peter Sivilay <sivilaypeter@gmail.com>
2016-11-18 13:22:55 -0600

    Initialize webpage with css, js directory and index html and readme, add jsfiddle html and link it to scripts js file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var number1  = parseInt(prompt("Please enter a number:"));
+var number2 = parseInt(prompt("And now please enter another number:"));
+
+alert(add(number1, number2));
